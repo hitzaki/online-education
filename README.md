61c945a45f912fda706cb2e9ab667acff4d0c4c8
lixc27 <1152849733@qq.com>
2025-01-22 20:39:25 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # online-education
 在线教育平台后端
+## 目录结构
+api层
+bus层
+common层
+biz层
---
+++ src/main/java/com/git/hitzaki/education/MainApplication.java
@@ -0,0 +1,13 @@
+package com.git.hitzaki.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
