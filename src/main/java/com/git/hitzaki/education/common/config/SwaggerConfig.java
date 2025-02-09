ed3066800d195979ae2753b5e0c7b90ed2ba4e08
lixc27 <1152849733@qq.com>
2025-02-09 20:10:17 +0800

    feat: swagger使用

---
+++ src/main/java/com/git/hitzaki/education/common/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.git.hitzaki.education.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger配置
+ * @author hitzaki
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
+
