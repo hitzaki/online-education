13dcab67201c7461abb01b67e5695b031944ef8d
lixc27 <1152849733@qq.com>
2025-02-18 00:57:57 +0800

    feat: 认证授权2

--- src/main/java/com/git/hitzaki/education/common/config/SaTokenConfig.java
+++ src/main/java/com/git/hitzaki/education/common/config/SaTokenConfig.java
@@ -2,17 +2,26 @@ package com.git.hitzaki.education.common.config;
 
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.interceptor.SaAnnotationInterceptor;
 import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * satoken配置
  * @author hitzaki
  */
 @Configuration
-public class SaTokenConfig {
+public class SaTokenConfig implements WebMvcConfigurer {
+    // 注册Sa-Token的注解拦截器，打开注解式鉴权功能
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册注解拦截器，并排除不需要注解鉴权的接口地址 (与登录拦截器无关)
+        registry.addInterceptor(new SaAnnotationInterceptor()).addPathPatterns("/**");
+    }
 
     /**
      * 注册 [Sa-Token全局过滤器]
